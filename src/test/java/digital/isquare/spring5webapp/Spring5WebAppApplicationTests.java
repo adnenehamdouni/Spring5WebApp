72dd7055bb1e0d5d3f262af80214656b2f1ff9fe
Adnene HAMDOUNI <adnene.hamdouni@qugenx.com>
2022-01-26 07:33:01 +0100

    :construction: first commit

---
+++ src/test/java/digital/isquare/spring5webapp/Spring5WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package digital.isquare.spring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5WebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
