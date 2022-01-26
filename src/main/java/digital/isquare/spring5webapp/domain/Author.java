52e9fcb4ff7c8ee82f7627a222bc2c6d7bcfd4a1
Adnene HAMDOUNI <adnene.hamdouni@qugenx.com>
2022-01-26 08:10:25 +0100

    :construction: add manytomany relationship between author and book and create jointable

--- src/main/java/digital/isquare/spring5webapp/domain/Author.java
+++ src/main/java/digital/isquare/spring5webapp/domain/Author.java
@@ -1,9 +1,6 @@
 package digital.isquare.spring5webapp.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Set;
 
 @Entity
@@ -14,6 +11,9 @@ public class Author {
     private Long id;
     private String firstName;
     private String lastName;
+
+
+    @ManyToMany(mappedBy = "authors")
     private Set<Book> books;
 
     public Author() {
