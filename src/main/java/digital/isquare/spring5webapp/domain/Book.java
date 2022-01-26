de75717b06c67dbcd4d23e3a4d576cef539d6d03
Adnene HAMDOUNI <adnene.hamdouni@qugenx.com>
2022-01-26 08:01:28 +0100

    :construction: add JPA entities annotation and id generation

--- src/main/java/digital/isquare/spring5webapp/domain/Book.java
+++ src/main/java/digital/isquare/spring5webapp/domain/Book.java
@@ -1,9 +1,17 @@
 package digital.isquare.spring5webapp.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Set;
 
+@Entity
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String title;
     private String isbn;
     private Set<Author> authors;
@@ -18,11 +26,35 @@ public class Book {
         this.authors = authors;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getTitle() {
         return title;
     }
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
     }
 }
