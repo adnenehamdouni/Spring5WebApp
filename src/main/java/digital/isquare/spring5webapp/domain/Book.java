1d37df7f8621858b8b67157469a7e5f07a4e715a
Adnene HAMDOUNI <adnene.hamdouni@qugenx.com>
2022-01-26 08:23:09 +0100

    :construction: add toString, Equals and hashCode for Hibarnate

--- src/main/java/digital/isquare/spring5webapp/domain/Book.java
+++ src/main/java/digital/isquare/spring5webapp/domain/Book.java
@@ -1,6 +1,7 @@
 package digital.isquare.spring5webapp.domain;
 
 import javax.persistence.*;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -18,8 +19,8 @@ public class Book {
             inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors;
 
-    public Book(String title) {
-        this.title = title;
+    public Book() {
+
     }
 
     public Book(String title, String isbn, Set<Author> authors) {
@@ -58,5 +59,28 @@ public class Book {
 
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
